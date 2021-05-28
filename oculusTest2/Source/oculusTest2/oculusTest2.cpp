7e7e3428768b6688d53a1d9fa46176206071ea38
BitBaseBit <vardygreg23@gmail.com>
2021-05-28 13:48:24 +0100

    removed old unused files

--- oculusTest2/Source/oculusTest2/oculusTest2.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "oculusTest2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, oculusTest2, "oculusTest2" );
